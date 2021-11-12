a95b547f569566171b58c9f661157928d5d6c979
nawafnaofal <nawafnaufal1@gmail.com>
2021-11-12 17:05:40 +0700

    commit tugas pertemuan ke 3

---
+++ P2_1_1204036/Book_1204036.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204036
+{
+    public class Book_1204036: Product_1204036
+    {
+        protected string pageCount;
+        
+        public Book_1204036(string type, string title, string pagecount) 
+            : base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+        
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
---
+++ P2_1_1204036/Product_1204036.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204036
+{
+    public class Product_1204036
+    {
+        private string myType;
+        private string myTitle;
+
+        public Product_1204036()
+        {
+            
+        }
+
+        public Product_1204036(string type, string title)
+        {
+            this.myType = type;
+            this.myTitle = title;
+        }
+
+        public string MyType
+        {
+            get
+            {
+                return myType;
+            }
+
+            set
+            {
+                myType = value;
+            }
+        }
+        
+        public string MyTitle
+        {
+            get
+            {
+                return myTitle;
+            }
+
+            set
+            {
+                myTitle = value;
+
+            }
+        }                          
+    }
+}
+
+
+
+
